dacb4f97cbbd7e361741123b1bb7029110297879
Jeff McMillen <jeff.mcmillen@playableworlds.com>
2023-08-24 16:23:39 -0500

    Created the User Schema based on necessary parameters

---
+++ models/User.js
@@ -0,0 +1,56 @@
+// Import Schema and model from mongoose
+const { Schema, model } = require('mongoose');
+
+// Regular expression for matching and validating an email address for the user
+const regexEmail = '/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/'
+
+// Establishes Schema for creating the User model
+const userSchema = new Schema (
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            validate: function (validator) {
+                return regexEmail.test(validator);
+            }
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            },
+        ]
+    },
+    {
+      // Including virtuals in our response
+      toJSON: {
+        virtuals: true,
+      },
+      id: false,
+    }
+);
+  
+// Create a virtual property `friendCount` that returns the length of the friends array
+userSchema
+    .virtual('friendCount')
+    .get(function () {
+        return `You have a total of ${this.friends.length} friends!`;
+    });
+  
+// Initialize our User model
+const User = model('user', userSchema);
+
+module.exports = User;
