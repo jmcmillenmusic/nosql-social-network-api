9053013289acefc28ca220d27173b97afe86a4e3
Jeff McMillen <jeff.mcmillen@playableworlds.com>
2023-08-23 22:51:07 -0500

    Created initial file structure

---
+++ index.js
@@ -0,0 +1,20 @@
+// Declares initial required variables
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+// Establishes our port and instantiates Express
+const PORT = 3001;
+const app = express();
+
+// Connects the middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+// Listens for the port and sends that information to the console
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server for ${activity} running on port ${PORT}!`);
+  });
+});
