19c5e0cb79081f77de003942dc14814d69c28a98
Jeff McMillen <jeff.mcmillen@playableworlds.com>
2023-08-27 21:46:04 -0500

    Added createFriend and deleteFriend functions to userController.js and to userRoutes.js

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,7 +5,9 @@ const {
     getSingleUser,
     createUser,
     updateUser,
-    deleteUser
+    deleteUser,
+    createFriend,
+    deleteFriend,
 } = require('../../controllers/userController');
 
 // API Route: /api/users
