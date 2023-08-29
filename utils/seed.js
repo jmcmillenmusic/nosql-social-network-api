c24e506593a6148465d2f491487dbf08a0c8bc54
Jeff McMillen <jeff.mcmillen@playableworlds.com>
2023-08-29 09:58:48 -0500

    Cleaned up seed.js, fixed errors in User and Thought models to allow routes to run correctly

--- utils/seed.js
+++ utils/seed.js
@@ -9,12 +9,10 @@ connection.once('open', async () => {
   // Clear out all previous data in order to start fresh
   await User.deleteMany({});
   await Thought.deleteMany({});
-  // await Reaction.deleteMany({});
 
   // Initialize empty arrays to contain our initial lists of users, thoughts, and reactions
   const usersList = [];
   const thoughtsList = [];
-  // const reactionsList = [];
   
   // Add objects containing the usernames and email of all users to usersList
   for (let i = 0; i < users.length; i++) {
@@ -29,7 +27,6 @@ connection.once('open', async () => {
       email: newUser.email
     });
   };
-  // console.log(usersList);
   
   // Add objects containing the thoughtText and usernames of all thoughts to thoughtsList
   for (let i = 0; i < thoughts.length; i++) {
@@ -55,33 +52,6 @@ connection.once('open', async () => {
       { new: true },
       );
   };
-  // console.log(thoughtsList);
-
-  // Add objects containing the reactionBody and usernames of all reactions to reactionsList
-  // for (let i = 0; i < reactions.length; i++) {
-  //   const reactionObject = {
-  //     reactionBody: reactions[i].reactionBody,
-  //     username: reactions[i].username
-  //   };
-    // const newReaction = await Reaction.create(reactionObject);
-    // reactionsList.push({
-    //   _id: newReaction._id.toString(),
-    //   reactionBody: newReaction.reactionBody,
-    //   username: newReaction.username
-    // });
-  // };
-  // console.log(reactionsList);
-
-  // for (let i = 0; i < reactionsList.length; i++) {
-  //   const userId = usersList.filter(
-  //     (user) => user.username === reactionsList[i].username
-  //   );
-  //   await User.findOneAndUpdate(
-  //     { _id: userId[0]._id },
-  //     { $push: { reactions: reactionsList[i]._id } },
-  //     { new: true },
-  //   );
-  // };
 
   // Check to see that everything is seeded
   console.table(users);
